07c2250ddc02c12505d144ff854e9cb06d016d3f
SokovianSquirrelly <104170505+SokovianSquirrelly@users.noreply.github.com>
2022-06-16 15:01:55 -0600

    Added text functionality

--- Login.js
+++ Login.js
@@ -1,9 +1,24 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+
+async function sendText(phoneNumber)
+{
+  console.log("Phone Number:", phoneNumber);
+  await fetch("https://dev.stedi.me/twofactorlogin/"+phoneNumber,
+  {
+    method: "POST",
+    headers:
+    {
+      "content-type":"application/text"
+    }
+  })
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
 
   return (
     <SafeAreaView style={styles.margin}>
@@ -11,17 +26,32 @@ const Login = () => {
         style={styles.input}
         onChangeText={setPhoneNumber}
         value={phoneNumber}
-        placeHolder="123-456-7890"
+        placeholder="123-456-7890"
+        placeholderTextColor="#D3D3D3"
         keyboardType="numeric"
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Send Text</Text>
+      </TouchableOpacity>
+
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1 1 1 uhhh... 1"
+        placeholderTextColor="#D3D3D3"
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{console.log("Login button is clicked.")}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -33,7 +63,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
-  margin:{marginTop:100}
+  margin:{marginTop:100},
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+
+
 });
 
 export default Login;
